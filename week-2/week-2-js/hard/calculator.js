4b2b0d9a66a666f698b2fafb4e9d5b9bfc933426
Kushal3502 <kushal2002personal@gmail.com>
2024-08-13 15:50:01 +0530

    week-2-js

--- week-2/week-2-js/hard/calculator.js
+++ week-2/week-2-js/hard/calculator.js
@@ -16,6 +16,43 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    this.result += num;
+  }
+
+  subtract(num) {
+    this.result -= num;
+  }
+
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num == 0) throw new Error("Invalid");
+    this.result /= num;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(num) {
+    this.result = eval(num.replace(/\s+/g, " "));
+    if (this.result === Infinity) {
+      throw new Error("Infinity");
+    }
+    return this.result;
+  }
+}
 
 module.exports = Calculator;
